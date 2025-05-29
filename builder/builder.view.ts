960c64de36b13d96775df45614bbc86c53eedeb4
Alexey Yurchenko <alexes.dev@gmail.com>
2025-05-29 07:54:55 +0000

    feat: implement right model

--- builder/builder.view.ts
+++ builder/builder.view.ts
@@ -1,6 +1,5 @@
 namespace $.$$ {
-	type Item = {
-		id: number
+	type NewItem = {
 		excercise: string
 		sets: number
 		reps: number
@@ -11,18 +10,35 @@ namespace $.$$ {
 
 	const data_key = 'gymload_builder_data'
 
+	const build_key = ( s: string ) => `gymload_builder_v1_${ s }`
+
+	const empty_item: NewItem = {
+		excercise: '',
+		sets: 3,
+		reps: 12,
+		begin_weight: 20,
+		finish_weight: 50,
+		min_step: 5,
+	}
+
 	export class $gymload_builder extends $.$gymload_builder {
-		@$mol_mem
-		data( next?: Item[] ): Item[] {
-			return this.$.$mol_state_local.value<Item[]>( data_key, next ) || []
+		data_ids( next?: number[] ): number[] {
+			return this.$.$mol_state_local.value( this.state_key( 'excercise_ids' ), next ) || []
 		}
 
 		override rows() {
-			return this.data().map( item => this.Row( item.id ) )
+			return this.data_ids().map( id => this.Row( id ) )
 		}
 
-		row( id: any ) {
-			return this.data()[ id - 1 ]
+		row( id: any, next?: NewItem ): NewItem {
+			const key = this.state_key( `excercise_${ id }` )
+			if( next === undefined ) {
+				return this.$.$mol_state_local.value<NewItem>( key ) || empty_item
+			}
+
+			this.$.$mol_state_local.value<NewItem>( key, next )
+
+			return next
 		}
 
 		@$mol_mem_key
@@ -43,66 +59,80 @@ namespace $.$$ {
 
 		override week_labels( id: any ): readonly ( string )[] {
 			const weights = this.plan( id )
-			return weights.map( ( w, i ) => `${i+1}: ${w}` )
+			return weights.map( ( w, i ) => `${ i + 1 }: ${ w }` )
 		}
 
 		override row_exercise_title( id: any ) {
-			const r = this.row( id )
-			return `${ r.excercise } #${ r.id }`
+			return `Exercise #${ id }`
 		}
 
 		override row_remove( id: any ) {
-			this.data( this.data().filter( item => item.id !== id ) )
+			this.data_ids( this.data_ids().filter( item => item !== id ) )
 		}
 
-		change_field<K extends keyof Item>(key: K, id: any, next?: Item[K]): Item[K] {
-			const row = this.row( id )
-
+		change_field<K extends keyof NewItem>( key: K, id: any, next?: NewItem[ K ] ): NewItem[ K ] {
 			if( next !== undefined ) {
-				const newRow = { ...row, [key]: next }
-				this.data( this.data().map( item => item.id === id ? newRow : item ) )
+				const newRow = this.row( id, {
+					...this.row( id ),
+					[ key ]: next,
+				} )
+
+				return newRow[ key ]
 			}
 
-			return row[ key ]
+			return this.row( id )[ key ]
 		}
 
+		@$mol_mem_key
 		override row_exercise( id: any, next?: string ) {
 			return this.change_field( 'excercise', id, next )
 		}
 
+		@$mol_mem_key
 		override row_sets( id: any, next?: number ) {
 			return this.change_field( 'sets', id, next )
 		}
 
+		@$mol_mem_key
 		override row_reps( id: any, next?: number ) {
 			return this.change_field( 'reps', id, next )
 		}
 
+		@$mol_mem_key
 		override row_begin_weight( id: any, next?: number ) {
 			return this.change_field( 'begin_weight', id, next )
 		}
 
+		@$mol_mem_key
 		override row_finish_weight( id: any, next?: number ) {
 			return this.change_field( 'finish_weight', id, next )
 		}
 
+		@$mol_mem_key
 		override row_min_step( id: any, next?: number ) {
 			return this.change_field( 'min_step', id, next )
 		}
 
+		@$mol_mem
+		new_id() {
+			return Math.max( 1, 1 + Math.max( ... this.data_ids() ) )
+		}
+
 		override add_exercise() {
-			this.data( [
-				...this.data(),
-				{
-					id: this.data().length + 1,
-					excercise: 'Приседания',
-					sets: 3,
-					reps: 10,
-					begin_weight: 50,
-					finish_weight: 100,
-					min_step: 5,
-				},
-			] )
+			const ids = this.data_ids()
+			const new_id = this.new_id()
+			const last_id = ids[ ids.length - 1 ]
+			const new_item: NewItem = { ...empty_item }
+
+			if( last_id !== undefined ) {
+				const last_row = this.row( last_id )
+				new_item.sets = last_row.sets
+				new_item.reps = last_row.reps
+				new_item.min_step = last_row.min_step
+			}
+
+			this.data_ids( [ ...this.data_ids(), new_id ] )
+			this.row( new_id, new_item )
 		}
 	}
 }
